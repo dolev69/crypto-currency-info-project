a0b8d39dc1c2d5d1a596fac1d06df16b38ffbd06
dolev69 <starixbattels@gmail.com>
2023-07-27 15:59:26 +0300

    -spaces + fixed live report callouts every secound

--- main.js
+++ main.js
@@ -13,70 +13,45 @@ $(() => {
   currenciesLink.addEventListener("click", displayCurrencies);
   
   async function displayCurrencies() {
-
     clearInterval(interval);
-
     try {
-
       const spinner = document.getElementById("spinner"); // loading spinner (modified in css).
-
       spinner.style.display = "inline-block"; // shows the loading spinner.
-
       const storedCoinsData = sessionStorage.getItem("coinsData"); // gets the data from session storage.
       let coins;
 
       if (storedCoinsData) { // if we already have the data then we just print the coins
-
         coins = JSON.parse(storedCoinsData);
         printCoins(coins);
-
       }
 
       else {
-
         coins = await getJson("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1"); // api call
         printCoins(coins);
-
         saveToSessionStorage(coins); // saving to session storage - incase of reload window.
-
       }
-    }
-
-    catch (err) {
+    } catch (err) {
       console.log(`Error displaying the currencies: ${err}`);
-    }
-
-    finally {
+    } finally {
       spinner.style.display = "none"; // hides the loading spinner when done.
-    }
+    }}
   
-  }
-  
-
-  ///// BONUS - live reports graph //////
   // diplays the live reports page when clicked
   liveReportsLink.addEventListener("click", displayLiveReports);
   
   async function displayLiveReports() {
-
     const checkedCoins = JSON.parse(sessionStorage.getItem("checkedCoins")); // all the checked coins
-
     const coinDataPoints = {};
 
     if (checkedCoins) {
-
     // displays 2 buttons and the live report div
     mainContent.innerHTML = `<div id="chartContainer" style="height: 370px; width: 100%; background-color: white;"></div>`;
-
     async function getHistoricalData(coin) { // just to get some info about the past USD value to make a nice display in the graph instead of single dot of current price.
       
       const today = new Date();
-    
       const toTs = Math.floor(today.getTime() / 1000);
-    
       const endpoint = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${coin}&tsym=USD&limit=7&toTs=${toTs}&aggregate=1`; // api link
-    
-      // Fetch the historical data
+
       const response = await fetch(endpoint); // api call to the history data
       const data = await response.json();
       return data.Data.Data;
@@ -163,7 +138,6 @@ function toggleDataSeries(e) {
         for (const coin of checkedCoins) {
 
           const liveCoins = await getJson(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coin}&tsyms=USD`); // api call to the checked coin USD value
-    
           const usdValue = liveCoins[coin].USD; // USD value of every coin
 
           if (!coinDataPoints[coin]) {
@@ -172,14 +146,12 @@ function toggleDataSeries(e) {
           
           // Add the data point for the current coin to the array
           coinDataPoints[coin].push({ x: Date.now(), y: usdValue });
-          
           if (coinDataPoints[coin].length > 10) {
             coinDataPoints[coin].shift(); // Remove the first data point (oldest data point) from the array.
           }
         }
           
-      }, 2000);
-
+      }, 4000);
     } else {
       mainContent.innerHTML = `<h2>SELECT 1-5 COINS TO ACTIVATE LIVE REPORT`
     }
@@ -191,32 +163,19 @@ function toggleDataSeries(e) {
   function displayAbout() {
 
     clearInterval(interval);
-
     mainContent.innerHTML =
-      `
-        <div class="about">
-
+      `<div class="about">
           <h1>About me</h1>
-
           <img src="assets/profilePic.png" width="200">
-
           <p>
-
             Dolev Mizrachi<br><br>
-  
             dolevmizrachii@gmail.com<br><br>
-
             <div class="social-media">
-
               <a href="https://github.com/zdolevm" target="_blank"><i class='bx bxl-github' ></i></a>
-
             </div>
-
             MADE BY DOLEVM
-
           </p>
-        </div>
-      `
+        </div>`
   }
 
   // saving to session storage to prevent spamming the api
@@ -229,7 +188,6 @@ function toggleDataSeries(e) {
     }
 
   function getFromSessionStorageCheckedCoins() { // getting the stored data of checked coins
-
     const getItem = sessionStorage.getItem("checkedCoins");
 
     if (getItem) {
@@ -240,10 +198,8 @@ function toggleDataSeries(e) {
 
   // gets the data from the api and returns json of the data
   async function getJson(url) {
-
     try {
       const response = await fetch(url);
-
       if (!response.ok) {
         throw new Error("response is not okay"); // throws error when response is not okay.
       }
@@ -255,56 +211,33 @@ function toggleDataSeries(e) {
       console.error("Error fetching JSON:", error);
       throw error;
     }
-
   }
 
   // prints the coins in cards (card container from bootstrap)
   function printCoins(coins) {
-
     const mainContent = document.getElementById("mainContent"); // main content div.
     let html = "";
 
     for (const coin of coins) { // looping through every coin from the api.
-            html +=
-              `
-              <div class="cards-container">
-
-                <div class="card">
-
-                  <img class="card-img-top" src="${coin.image}">
-
-                    <div class="card-body">
-
-                      <h5 class="card-title">${coin.symbol.toUpperCase()}</h5>
-                      
-                      <p class="card-text">${coin.name}</p>
-
-                        <div class="form-check form-switch">
-
-                          <input class="form-check-input" type="checkbox" role="switch" id="${coin.market_cap_rank}" switch="${coin.symbol.toUpperCase()}">
-
-                          <label class="form-check-label" for="flexSwitchCheckDefault"></label>
-
-                        </div>
-
-                        <br>
-
-                        <a class="btn btn-success" data-toggle="collapse" data-coin-name="${coin.id}" id="collapseBtn${coin.market_cap_rank}" role="button" aria-expanded="false" aria-controls="collapseExample">More Info</a><div class="spinner" id="spinnerId${coin.market_cap_rank}"></div>
-
-                        <div class="collapse" id="collapseId${coin.market_cap_rank}"></div>
-
-                        <div class="moreInfo" id="moreInfoContent-${coin.market_cap_rank}"></div>
-
+      html +=
+        `<div class="cards-container">
+          <div class="card">
+            <img class="card-img-top" src="${coin.image}">
+              <div class="card-body">
+                <h5 class="card-title">${coin.symbol.toUpperCase()}</h5>   
+                  <p class="card-text">${coin.name}</p>
+                    <div class="form-check form-switch">
+                      <input class="form-check-input" type="checkbox" role="switch" id="${coin.market_cap_rank}" switch="${coin.symbol.toUpperCase()}">
+                      <label class="form-check-label" for="flexSwitchCheckDefault"></label>
                     </div>
-
+                    <br>
+                    <a class="btn btn-success" data-toggle="collapse" data-coin-name="${coin.id}" id="collapseBtn${coin.market_cap_rank}" role="button" aria-expanded="false" aria-controls="collapseExample">More Info</a><div class="spinner" id="spinnerId${coin.market_cap_rank}"></div>
+                    <div class="collapse" id="collapseId${coin.market_cap_rank}"></div>
+                    <div class="moreInfo" id="moreInfoContent-${coin.market_cap_rank}"></div>
                 </div>
-
               </div>
-
-              `
-            
+            </div>`
     }
-
     mainContent.innerHTML = html;
     
     for (const coin of coins) {
@@ -325,22 +258,17 @@ function toggleDataSeries(e) {
         let moreInfoCoinData;
 
         if (isHidden) {
-
           if (savedSessionStorageMoreInfo) {
-
             moreInfoCoinData = JSON.parse(savedSessionStorageMoreInfo);
             const currentTime = new Date().getTime();
             
             if (currentTime > moreInfoCoinData.expDate) { // if the saved data is older than 2 minutes
-
               moreInfoCoinData = await getJson(`https://api.coingecko.com/api/v3/coins/${coinName}`); // new api call
               moreInfoCoinData.expDate = new Date().setMinutes(new Date().getMinutes() + 2); // adding expDate
               sessionStorage.setItem(coinName, JSON.stringify(moreInfoCoinData)); // updating session storage
-
             }
           
             if (moreInfoCoinData) {
-            
             $(`#collapseId${coin.market_cap_rank}`).html(`
                   ILS = ₪${moreInfoCoinData.market_data.current_price.ils}
                     <br>
@@ -352,7 +280,6 @@ function toggleDataSeries(e) {
           }
 
           else {
-
             moreInfoCoinData = await getJson(`https://api.coingecko.com/api/v3/coins/${coinName}`);
             moreInfoCoinData.expDate = new Date().setMinutes(new Date().getMinutes() + 2);
 
@@ -365,7 +292,6 @@ function toggleDataSeries(e) {
                   EUR = €${moreInfoCoinData.market_data.current_price.eur}
                 `
             )
-
             sessionStorage.setItem(coinName, JSON.stringify(moreInfoCoinData));
          }
         }
@@ -374,13 +300,11 @@ function toggleDataSeries(e) {
       });
       
     }
-
     let checkedCoins = getFromSessionStorageCheckedCoins() || []; // array of all the checked coins (default to an empty array if not found)
     let coinsList = $("#coinsList"); // div where all the checked coins will be displayed
   
     for (const coin of coins) {
       const switchButton = document.getElementById(coin.market_cap_rank);
-  
       switchButton.addEventListener("change", function () {
         const switchValue = this.getAttribute("switch");
   
@@ -389,11 +313,8 @@ function toggleDataSeries(e) {
             showLimitReachedModal(checkedCoins, coins); // Show the modal with the selected coins
             this.checked = false; // Uncheck the switch button if the limit is reached
           }
-  
           checkedCoins.push(switchValue); // pushing the checked coin to the checkedCoins array
-
         } else {
-
           const index = checkedCoins.indexOf(switchValue); // getting the index of the unchecked coin;
   
           if (index !== -1) {
@@ -410,15 +331,12 @@ function toggleDataSeries(e) {
         coinsList.html("<span>Coins Selected:</span> " + checkedCoins.join(" / ")); 
       }
     }
-
   }
 
   // modal popping up after 5 checked coins
   const limitReachedModal = document.getElementById("limitReachedModal");
   function showLimitReachedModal(checkedCoins, coins) {
-
     let html = "";
-
     for (const coin of checkedCoins) {
       html += 
         `<div><h5 class="card-title">${coin.toUpperCase()}</h5>
@@ -438,21 +356,21 @@ function toggleDataSeries(e) {
     const limitReachedModalCloseBtn = limitReachedModal.querySelector(".btn-secondary");
     limitReachedModalCloseBtn.addEventListener("click", function () {
       
-      if (checkedCoins.length > 5) {
-        checkedCoins.pop();
-      }
-      saveToSessionStorageCheckedCoins(checkedCoins);
-      $(limitReachedModal).modal("hide");
-      printCoins(coins);
+    if (checkedCoins.length > 5) {
+      checkedCoins.pop();
+    }
+    saveToSessionStorageCheckedCoins(checkedCoins);
+    $(limitReachedModal).modal("hide");
+    printCoins(coins);
     }); 
 
     $(limitReachedModal).on("hidden.bs.modal", function () {
 
-      if (checkedCoins.length > 5) {
-        checkedCoins.pop();
-        saveToSessionStorageCheckedCoins(checkedCoins);
-        printCoins(coins);
-      }
+    if (checkedCoins.length > 5) {
+      checkedCoins.pop();
+      saveToSessionStorageCheckedCoins(checkedCoins);
+      printCoins(coins);
+    }
     });
 
     for (const coin of checkedCoins) {
@@ -485,7 +403,6 @@ function toggleDataSeries(e) {
     const userSearch = searchInput.value; // what the user types for search
 
     searchCoins(userSearch); // searching what the user types
-
   }
 
   async function searchCoins(userSearch) {
@@ -493,16 +410,10 @@ function toggleDataSeries(e) {
     let coins;
 
     if (storedCoinsData) { // if we already have the data then we just print the coins
-
       coins = JSON.parse(storedCoinsData);
       printCoins(coins);
-
-    }
-
-    else {
-
+    } else {
       coins = await getJson("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1"); // api call
-
     }
 
     const filteredCoins = coins.filter((coin) => {
@@ -514,7 +425,5 @@ function toggleDataSeries(e) {
     } else {
       printCoins(filteredCoins); // prints the coins of what user searched.
     }
-
-  }
-        
+  }    
 })
